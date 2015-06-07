7abeef8952b5f44f07f1b015b9d0fa295125855f
Shalisska <alena.simonova83@gmail.com>
2015-06-08 01:39:29 +0300

    timer

--- app/scripts/common.js
+++ app/scripts/common.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-$(function() {
 	$('.chart').easyPieChart({
 		scaleColor: false,
 		trackColor: '#e5e5e5',
@@ -8,5 +7,35 @@ $(function() {
 		lineCap: 'butt',
 		size: 110
 	});
-});
+	
+	var date = new Date();
+	var sec_chart, min_chart, hour_chart, day_chart;
+
+	var chartind = function() {
+
+	var date_new = new Date();
+
+	if (date != date_new) {
+		date = new Date();
+		
+		var mlsec = date.getMilliseconds();
+		var sec = date.getSeconds();
+		var min = date.getMinutes();
+		var hour = date.getHours();
+
+		sec_chart = parseInt(100 / 1000 * mlsec);
+		min_chart = parseInt(100 / 60 * sec);
+		hour_chart = parseInt(100 / 60 * min);
+		day_chart = parseInt(100 / 24 * hour);
+
+		$('#sec').data('easyPieChart').update(sec_chart);
+		$('#min').data('easyPieChart').update(min_chart);
+		$('#hour').data('easyPieChart').update(hour_chart);
+		$('#day').data('easyPieChart').update(day_chart);
+	}
+	
+	setTimeout(chartind, 100);
+	};
+	
+chartind();
 });
